47072d20e20c6a8f464d0e811d6b07054b326fc0
Martin Thuresson <martin.thure@gmail.com>
2023-05-19 06:50:39 +0200

    Added data flow

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sqlite3
-import bottle
+import json
 from gevent.pywsgi import WSGIServer
+from gevent import monkey
+monkey.patch_all()
+
 con = sqlite3.connect("feedback.db")
 cur = con.cursor()
 
@@ -10,35 +13,122 @@ cur.execute("""CREATE TABLE IF NOT EXISTS exempel (
   elev TEXT NULL,
   uppgift TEXT NULL,
   typ TEXT NULL,
-  beskrivning TEXT NULL
+  beskrivning TEXT NULL,
+  PRIMARY KEY (elev, uppgift, typ)
 );""")
 
 cur.execute("""CREATE TABLE IF NOT EXISTS svar (
-  exempelid
+  exempelid INTEGER NULL,
   elev TEXT NULL,
-  svar TEXT NULL
+  svar TEXT NULL,
+  feedback TEXT NULL,
+  PRIMARY KEY (exempelid, elev)
 );""")
 
 con.commit()
 
 def application(env, start_response):
-    if env['PATH_INFO'] == '/examples':
-      """/examples
-      POST = add / update
-      updatera = radera alla svar"""
-      """
-      res = cur.execute("SELECT score FROM ")
-      res.fetchall()
-      """
+    jsondata = {}
+    if env['REQUEST_METHOD'] == "POST":
+        length= int(env.get('CONTENT_LENGTH', '0'))
+        if length!=0 and ("application/json" in env.get('CONTENT_TYPE', '')):
+          jsondata = json.loads(env['wsgi.input'].read(length))
+        else:
+          start_response('400 Bad Request', [('Content-Type', 'application/json')])
+          return """{"error":"json required"}"""
+
+    if env['PATH_INFO'] == '/exemepel':
+      if env['REQUEST_METHOD'] == "POST":
+        values = (
+          jsondata["elev"],
+          jsondata["uppgift"],
+          jsondata["typ"],
+          jsondata["beskrivning"],
+          jsondata["beskrivning"]
+        )
+        cur.execute("""
+          INSERT INTO exempel(elev, uppgift, typ, beskrivning)
+            VALUES(?,?,?,?)
+          ON CONFLICT(elev, uppgift, typ) 
+            DO UPDATE SET beskrivning=?;
+          """, values);
+        con.commit()
+
+        values = (
+          jsondata["elev"],
+          jsondata["uppgift"],
+          jsondata["typ"]
+        )
+        res  = cur.execute("""
+          SELECT rowid FROM exempel 
+            WHERE elev = ? AND uppgift = ? AND typ = ?;""",
+          values)
+        id = res.fetchall()[0][0]
+        cur.execute("""
+          DELETE FROM svar WHERE exempelid = ?;""", (id,));
+        con.commit()
+
+        start_response('200 OK', [('Content-Type', 'application/json')])
+        return [bytes(json.dumps({"id":id}), 'utf-8')]
+      if env['REQUEST_METHOD'] == "GET":
+        res  = cur.execute("""
+          SELECT rowid, elev, uppgift, typ, beskrivning FROM exempel;"""
+          )
+        def tillDict(rad):
+          return {
+          "id":rad[0],
+          "elev":rad[1],
+          "uppgift":rad[2],
+          "typ":rad[3],
+          "beskrivning":rad[4]
+          }
+        allaexempel = list(map(tillDict,res.fetchall()))
+        start_response('200 OK', [('Content-Type', 'application/json')])
+        return [bytes(json.dumps({"exempel":allaexempel}), 'utf-8')]
 
     elif env['PATH_INFO'] == '/svar':
-      """/svar
-      POST = add
-      GET = alla från uppgift"""
-      """
-      res = cur.execute("SELECT score FROM ")
-      res.fetchall()
-      """
+      if env['REQUEST_METHOD'] == "POST":
+        values = (
+          jsondata["id"],
+          jsondata["elev"],
+          jsondata["svar"],
+          jsondata["feedback"],
+          jsondata["svar"],
+          jsondata["feedback"]
+        )
+        cur.execute("""
+          INSERT INTO svar(exempelid, elev, svar, feedback)
+            VALUES(?,?,?,?)
+          ON CONFLICT(exempelid, elev) 
+            DO UPDATE SET svar = ?, feedback = ?;
+          """, values);
+        con.commit()
+        values = (
+          jsondata["id"],
+          jsondata["elev"],
+        )
+        res  = cur.execute("""
+          SELECT rowid FROM svar 
+            WHERE exempelid = ? AND elev = ?;""",
+          values)
+        id = res.fetchall()[0][0]
+
+        start_response('200 OK', [('Content-Type', 'application/json')])
+        return [bytes(json.dumps({"id":id}), 'utf-8')]
+      if env['REQUEST_METHOD'] == "GET":
+        res  = cur.execute("""
+          SELECT rowid, exempelid, elev, svar, feedback FROM svar;"""
+          )
+        def tillDict(rad):
+          return {
+          "id":rad[0],
+          "elev":rad[1],
+          "svar":rad[2],
+          "feedback":rad[3]
+          }
+        allaexempel = list(map(tillDict,res.fetchall()))
+        start_response('200 OK', [('Content-Type', 'application/json')])
+        return [bytes(json.dumps({"exempel":allaexempel}), 'utf-8')]
       start_response('200 OK', [('Content-Type', 'text/html')])
       return [b"<b>hello world</b>"]
 
